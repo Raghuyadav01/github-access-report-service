06b8055f409843738c0f2f0ecd8c079fda5848b3
Raghuyadav01 <yadavraghu7653@gmail.com>
2026-03-14 20:43:38 +0530

    Create GithubReportApplication as Spring Boot entry point

---
+++ src/main/java/com/example/githubreport/controller/GithubReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubReportApplication.class, args);
+    }
+
+}
